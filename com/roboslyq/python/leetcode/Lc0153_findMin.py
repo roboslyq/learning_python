f5a1e63080f2677a4a8048513a3d183aac53b221
roboslyq <robosloveu@163.com>
2019-10-18 17:26:06 +0800

    updated

--- com/roboslyq/python/leetcode/Lc0153_findMin.py
+++ com/roboslyq/python/leetcode/Lc0153_findMin.py
@@ -1,5 +1,6 @@
 from typing import List
 
+
 class Solution:
     def findMin(self, nums: List[int]) -> int:
         size = len(nums)
@@ -12,15 +13,15 @@ class Solution:
         while left < right:
             middle = (right + left) >> 1
             if nums[middle] > nums[right]:
-                left = middle+1
+                left = middle + 1
             else:
-                #assert nums[middle] < nums[right]
+                # assert nums[middle] < nums[right]
                 right = middle
         return nums[left]
 
 
-nums = [9,8,7,1,2,3]
-nums2 = [3,4,5,1,2]
+nums = [9, 8, 7, 1, 2, 3]
+nums2 = [3, 4, 5, 1, 2]
 so = Solution()
 print(so.findMin(nums))
 print(so.findMin(nums2))
