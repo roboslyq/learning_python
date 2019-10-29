8d7c14ed9fc10fbcd21fc14f78117528aebb0772
roboslyq <robosloveu@163.com>
2019-10-29 21:42:25 +0800

    updated

--- com/roboslyq/python/learn/spider/geek_spider.py
+++ com/roboslyq/python/learn/spider/geek_spider.py
@@ -1,20 +1,29 @@
 # coding=utf-8
 import logging
 import os
-import re
+import re                           # 正则表达式包
 import time
 
-import pdfkit
-import requests
-from PyPDF2 import PdfFileMerger
-from bs4 import BeautifulSoup
+import pdfkit                       # pdf 工具包
+import requests                     # 网络请求包
+from PyPDF2 import PdfFileMerger    # pdf 合并工具包
+from bs4 import BeautifulSoup       # html解析处理包
 
+'''
+python 爬虫，可以抓取网络上的文章并且保存为Pdf
+'''
+# 定义网页入口路径
+domain_path = "https://www.liaoxuefeng.com"
+base_path = "/wiki/1016959663602400"
+
+# 定义请求头，如果未定义可能导致https请求报503异常
 headers = {"X-Member-Id": "23832170000",
            "X-Region": "1100000",
            "User-Agent": "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 1.1.4322; .NET CLR 2.0.50727)",
            "X-Channel": "01",
            "Content-Type": "application/json;charset=UTF-8"}
 
+# html模板，用来保存下载的html内容
 html_template = """
                   <!DOCTYPE html>
                   <html lang="en">
@@ -28,19 +37,25 @@ html_template = """
                 """
 
 
+"""
+解析URL，返回HTML内容
+:param url:解析的url
+:param name: 保存的html文件名
+:return: html
+"""
+
+
 def parse_url_to_html(url, name):
-    """
-    解析URL，返回HTML内容
-    :param url:解析的url
-    :param name: 保存的html文件名
-    :return: html
-    """
     try:
         response = requests.get(url, headers=headers)
         soup = BeautifulSoup(response.content, 'html.parser')
-        print(soup.prettify())
+        # print(soup.prettify())
         # 正文
         # body = soup.find_all(class_="x-wiki-content")[0]
+        """
+        以下是根据需要下载的内容所在标签查询所有内容.具体标签内容需要根据实际情况来定。
+        不同的页面标签可能不一样
+        """
         body = soup.find_all(id="x-content")[0]
         # 标题
         title = soup.find('h4').get_text()
@@ -51,8 +66,9 @@ def parse_url_to_html(url, name):
         center_tag.insert(1, title_tag)
         body.insert(1, center_tag)
         html = str(body)
-        # body中的img标签的src相对路径的改成绝对路径,注意"? src"之间有一个空格
+        # body中的img标签的src相对路径的改成绝对路径,注意"? src"之间有一个空格(因为有一个属性是data-src,如果不加空格会匹配到这一个属性)
         pattern1 = "(<img .*? src=\")(.*?)(\")"
+        # iframe和img一样，如果src属性包含相对路径，则需要处理为绝对路径。否则会导致生成pdf时报NotFoundContent异常
         pattern2 = re.compile(r'(<iframe)(.*?)(</iframe>)')
 
         def func1(m):
@@ -63,8 +79,9 @@ def parse_url_to_html(url, name):
                 return m.group(1) + m.group(2) + m.group(3)
 
         html = re.compile(pattern1).sub(func1, html)
+        # 删除iframe元素
         html = pattern2.sub(r'', html)
-        print(html)
+        # print(html)
         html = html_template.format(content=html)
         html = html.encode("utf-8")
         with open(name, 'wb') as f:
@@ -79,8 +96,7 @@ def get_url_list():
     获取所有URL目录列表
     :return:
     """
-
-    response = requests.get("https://www.liaoxuefeng.com/wiki/1016959663602400", headers=headers)
+    response = requests.get(domain_path + base_path, headers=headers)
     soup = BeautifulSoup(response.content, "html.parser")
     # print(soup.prettify())
     # menu_tag = soup.find_all(class_="x-wiki-index-item")[1]
@@ -88,7 +104,7 @@ def get_url_list():
     urls = []
     for tag in menu_tag.find_all("a"):
         # if len(urls) < 3:  # 调度时限定3个文件
-        url = "https://www.liaoxuefeng.com" + tag.get('href')
+        url = domain_path + tag.get('href')
         urls.append(url)
     return urls
 
