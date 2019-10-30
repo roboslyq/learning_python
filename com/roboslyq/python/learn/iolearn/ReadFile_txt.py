c79f38c43e5023a6555a864cbc5583c8a8c740dd
roboslyq <robosloveu@163.com>
2019-10-30 23:43:30 +0800

    updated or added

--- com/roboslyq/python/learn/iolearn/ReadFile_txt.py
+++ com/roboslyq/python/learn/iolearn/ReadFile_txt.py
@@ -1,18 +1,25 @@
 """
 一次性读全部
 """
+import os
 
 
 def readfile_original():
     try:
         f = open('D:\\python_workspace\\test.txt', 'r', encoding='UTF-8')
         print(f.read())
-    finally:
-        if f:
-            f.close()
+    except FileNotFoundError:
+        print("文件不存在")
+        # finally:
+    # if f:
+    #     f.close()
 
 
 def readfile_new():
+    """
+    默认关闭文件流形式
+    :return:
+    """
     with open('D:\\python_workspace\\test.txt', 'r', encoding='UTF-8') as f:
         print(f.read())
 
@@ -27,17 +34,28 @@ def readLine():
         for line in f.readlines():
             print(line.strip())
 
+# 当前py文件所在的绝对路径
+curPath=os.path.abspath(os.path.dirname(__file__))
+print(curPath)
+
+# 当前项目根路径
+rootPath = curPath[:curPath.find("learning_python\\")+len("learning_python\\")]
+print(rootPath)
+# 获取xxx文件路径
+
+xxx_file_path = os.path.abspath(rootPath+"com\\__init__.py")
+
+print(xxx_file_path)
 # 传统语法
 
 
 readfile_original()
 
-
-#新语法
+# 新语法
 
 
 readfile_new()
-#按行读
+# 按行读
 
 
 readLine()
