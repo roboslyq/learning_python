8e6abc8122d71aee195509eb6c5cbd757014ee76
roboslyq <robosloveu@163.com>
2019-10-31 22:46:28 +0800

    updated or added

--- com/roboslyq/python/learn/grammar/functionLearn.py
+++ com/roboslyq/python/learn/grammar/functionLearn.py
@@ -29,3 +29,35 @@ res1, res2, res3 = calc1(val1, val2)
 print(res1)
 print(res2)
 print(res3)
+
+
+# 入参为一个任意个数，python将其作为一个元组处理，使用*标识
+
+def calc(int1, *ints):
+    return int1 + sum(ints)
+
+
+res4 = calc(1, 2, 3, 4, 5)
+res5 = calc(1, 2, 3, 4, 5, 6, 7, 8)
+print(res4)
+print(res5)
+
+
+# 入参为任意个数的关键字，即有变量名称的，python将其作为map处理，使用**标识
+
+def calc2(int1, int2, **keyvalue):
+    print(int1 + int2)
+    for key, val in keyvalue.items():
+        print(key + ":" + val)
+
+
+"""
+调用方式如下：使用k=v的方式，传入多个参数
+"""
+calc2(1, 2, k1="v1", k2="v2")
+
+"""
+lambda函数
+"""
+addLambda = lambda x, y: x + y
+print(addLambda(1, 2))
