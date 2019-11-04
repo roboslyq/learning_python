f28ac7fb48abc3a413dd338225cc133a1dad2396
roboslyq <robosloveu@163.com>
2019-11-04 17:24:15 +0800

    updated

--- com/roboslyq/python/learn/json/json_demo.py
+++ com/roboslyq/python/learn/json/json_demo.py
@@ -1,12 +1,15 @@
 import json
 
+""" 
+直接将 list保存到json文件中 ，文件的 形式还是list  
+"""
 data_list = [1, 2, 3, 4, 5, 6, ]
 with open("data.json", "w") as data_json:
     jsonObj = json.dump(data_list, data_json)
 
-user_info = {"id": "01", "name": "roboslyq"}
-data_info = {"phone": "110", "info": user_info}
 """
+将标准的 Key-value形式保存的json文件中，文件的具体格式为标准的json
+exp:
 {
   "phone": "110",
   "info": {
@@ -15,5 +18,8 @@ data_info = {"phone": "110", "info": user_info}
   }
 }
 """
+user_info = {"id": "01", "name": "roboslyq"}
+data_info = {"phone": "110", "info": user_info}
+
 with open("user.json", "w") as user_info:
     json.dump(data_info, user_info)
