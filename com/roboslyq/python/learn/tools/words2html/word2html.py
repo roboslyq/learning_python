daa709ca3a504ea05869db5d99e98e8a1bb84a5a
roboslyq <robosloveu@163.com>
2019-11-19 19:07:59 +0800

    优化调整：指定下载文件，合并PDF生成书签

---
+++ com/roboslyq/python/learn/tools/words2html/word2html.py
@@ -0,0 +1,15 @@
+from pydocx import PyDocX
+
+"""
+旧包：docx2html，此包要求python版本较低
+新包路径如下：
+https://github.com/CenterForOpenScience/pydocx
+"""
+
+docx_file = r'D:\十二刻度-个人信息及隐私政策.docx'
+html_fle = r'D:\result.html'
+html = PyDocX.to_html(docx_file)
+with open(html_fle, encoding='UTF-8', mode='w') as f:
+    f.write(html)
+    f.flush()
+    f.close()
